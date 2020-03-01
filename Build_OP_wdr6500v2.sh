e358b1dea3da4504a49d0d007c9fa0c01da6d33f
castiellian <51224589+castiellian@users.noreply.github.com>
2020-03-01 21:48:36 +0800

    update wdr6500v2

---
+++ Build_OP_wdr6500v2.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+
+# 定制默认IP
+sed -i 's/192.168.1.1/192.168.1.8/g' package/base-files/files/bin/config_generate
+
+# 替换默认Argon主题
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+
+# 添加第三方软件包
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
+
+
+
+
+#创建自定义配置文件 - OpenWrt-Netgear3800
+
+rm -f ./.config*
+touch ./.config
+
+#
+# ========================固件定制部分========================
+# 
+
+# 
+# 如果不对本区块做出任何编辑, 则生成默认配置固件. 
+# 
+
+# 以下为定制化固件选项和说明:
+#
+
+#
+# 有些插件/选项是默认开启的, 如果想要关闭, 请参照以下示例进行编写:
+# 
+#          =========================================
+#         |  # 取消编译VMware镜像:                   |
+#         |  cat >> .config <<EOF                   |
+#         |  # CONFIG_VMDK_IMAGES=y        |
+#         |  EOF                                    |
+#          =========================================
+#
+
+# 
+# 以下是一些提前准备好的一些插件选项.
+# 直接取消注释相应代码块即可应用. 不要取消注释代码块上的汉字说明.
+# 如果不需要代码块里的某一项配置, 只需要删除相应行.
+#
+# 如果需要其他插件, 请按照示例自行添加.
+# 注意, 只需添加依赖链顶端的包. 如果你需要插件 A, 同时 A 依赖 B, 即只需要添加 A.
+# 
+# 无论你想要对固件进行怎样的定制, 都需要且只需要修改 EOF 回环内的内容.
+# 
+
+# 编译Netgear3800固件:
+cat >> .config <<EOF
+CONFIG_TARGET_ar71xx=y
+CONFIG_TARGET_ar71xx_generic=y
+CONFIG_TARGET_DEVICE_ar71xx_generic_DEVICE_tl-wdr6500-v2=y
+CONFIG_BUSYBOX_DEFAULT_FEATURE_IPV6=y
+CONFIG_IPV6=y
+CONFIG_KERNEL_IPV6=y
+CONFIG_PACKAGE_autosamba=y
+CONFIG_PACKAGE_curl=y
+CONFIG_PACKAGE_ddns-scripts_no-ip_com=y
+CONFIG_PACKAGE_f2fsck=y
+CONFIG_PACKAGE_htop=y
+CONFIG_PACKAGE_nano=y
+CONFIG_PACKAGE_openssh-sftp-client=y
+CONFIG_PACKAGE_openssh-sftp-server=y
+CONFIG_PACKAGE_openssl-util=y
+CONFIG_PACKAGE_kmod-fs-f2fs=y
+CONFIG_PACKAGE_kmod-mt76=y
+CONFIG_PACKAGE_kmod-usb-storage=y
+CONFIG_PACKAGE_kmod-usb-storage-extras=y
+CONFIG_PACKAGE_kmod-usb-storage-uas=y
+CONFIG_PACKAGE_kmod-usb2=y
+CONFIG_PACKAGE_luci-app-webadmin=y
+CONFIG_PACKAGE_luci-app-ipsec-vpnd=y
+CONFIG_PACKAGE_luci-app-nps=y
+CONFIG_PACKAGE_luci-app-samba=y
+CONFIG_PACKAGE_luci-app-ttyd=y
+CONFIG_PACKAGE_ttyd=y
+CONFIG_PACKAGE_luci-app-p910nd=y
+CONFIG_PACKAGE_p910nd=y
+CONFIG_PACKAGE_luci-app-ssr-plus=y
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks=y
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Simple_obfs=y
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_V2ray_plugin=y
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_V2ray=y
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Trojan=y
+# CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Kcptun=y
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_ShadowsocksR_Server=y
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_ShadowsocksR_Socks=y
+CONFIG_PACKAGE_luci-app-nfs=y
+CONFIG_PACKAGE_nfs-kernel-server=y
+CONFIG_PACKAGE_portmap=y
+EOF
+
+# 
+# ========================固件定制部分结束========================
+# 
+
+
+sed -i 's/^[ \t]*//g' ./.config
+
+# 配置文件创建完成
